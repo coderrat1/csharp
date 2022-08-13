ac49efca5254ff45dc003662389c5999701051ef
coderrat1 <vermashubhamfy@gmail.com>
2022-08-13 16:57:45 +0530

    committing

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+
+namespace ExceptionHandling
+{
+    public class Exception2
+    {
+        public static void Main()
+        {
+            try
+            {
+                Stack st = new Stack();
+                st.Push(1);
+                st.Push(3);
+                st.Push(5);
+                st.Push(7);
+                st.Push(10);
+                foreach (Object obj in st)
+                {
+                    Console.WriteLine(obj);
+                }
+                Console.WriteLine("Total number of elements in stack : {0}", st.Count);
+                //st.Pop();
+                if (st.Count <= 0)
+                {
+                    throw new StackException("Stack is empty");
+                }
+                if (st.Count >= 5)
+                {
+                    throw new StackException("Stack is full you cant push elements");
+                }
+            }
+            catch (StackException ex)
+            {
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+            }
+        }
+        class StackException : Exception
+        {
+            public StackException() : base()
+            {
+
+            }
+            public StackException(string message) : base(message)
+            {
+
+            }
+        }
+    }
+}
