b0c1bfb6625c28a5f40937910bdc96e7e2dcfba0
coderrat1 <vermashubhamfy@gmail.com>
2022-08-13 16:18:40 +0530

    committing

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asssignments_C_sharp
+{
+    class Average_Marks
+    {
+        public static void Main()
+        {
+            int total = 0;
+            int greater_50 = 0;
+            int highmrks = 0;
+            Console.WriteLine("Enter Number of Students in class: ");
+            int students = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < students; i++)
+            {
+                Console.WriteLine("Enter Name of Student : ");
+                string studname = Console.ReadLine();
+
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("Enter your marks: ");
+                    int studmarks = int.Parse(Console.ReadLine());
+
+                    if (studmarks > 100 || studmarks < 0)
+                    {
+                        Console.WriteLine("Invalid marks entered, Enter the new marks again which is valid ");
+                        int newmarks = int.Parse(Console.ReadLine());
+                        studmarks = newmarks;
+                    }
+                    total += studmarks;
+
+                    if (studmarks > highmrks)
+                    {
+                        highmrks = studmarks;
+                    }
+                    if (studmarks > greater_50)
+                    {
+                        greater_50++;
+                    }
+
+                }
+                Console.WriteLine("Studnets Name : {0} and TotalMarks : {1} and HighestMarks : {2}", studname, total, highmrks);
+
+                double average = (total / 5.0) * 1.00;
+                Console.WriteLine("Average marks of Student {0} is {1}", studname, average);
+                Console.WriteLine("Marks having grater than 50 : {0}", greater_50);
+                total = 0;
+                greater_50 = 0;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Class2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asssignments_C_sharp
+{
+    class Lib_Books
+    {
+        enum bookType { Magazine = 0, Novel = 1, RefernceBook = 2, Miscellenious = 3 }
+        public static void Main(string[] args)
+        {
+            Books Book1;
+            Books Book2;
+            Books Book3;
+
+            Book1.bookID = 210;
+            Book1.title = "The Alchemist";
+            Book1.price = 259;
+            //string type = Convert.ToString((int)bookType.Magazine);
+            Enum type = bookType.Novel;
+
+            //Book1 specification
+            Console.WriteLine("Book1 ID: {0}", Book1.bookID);
+            Console.WriteLine("Book1 Title: {0}", Book1.title);
+            Console.WriteLine("Book1 Price: {0}", Book1.price);
+            Console.WriteLine("Book1 bookType : {0}", type.ToString());
+        }
+    }
+    struct Books
+    {
+        public int bookID;
+        public string title;
+        public int price;
+        //public void GetValue(int ID,string title,int price)
+        //{
+        //    this.bookID = ID;
+        //    this.title = title;
+        //    this.price = price;
+        //}
+
+    }
+}
